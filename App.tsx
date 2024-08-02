5de6609998141eee470edd61a49d499ee9974c0a
Alejo Saez <alejo.gebicki@gmail.com>
2024-08-02 12:22:27 -0300

    Latest Navigator Changes

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import HomeScreen from './src/screens/HomeScreen/Home';
-
+import ItemScreen from './src/screens/ItemScreen/itemScreen';
 const App: React.FC = () => {
   return <HomeScreen />;
+  // return <ItemScreen />
 };
 
 export default App;
