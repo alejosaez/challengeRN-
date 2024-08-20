b6de6b33127b83f9608e33c15095c742381a01a0
Alejo Saez <alejo.gebicki@gmail.com>
2024-08-20 19:56:21 -0300

    Fix: Added CORS configuration and updated Products component to test direct API fetch - Configured CORS in Express backend to allow requests from any origin - Updated Products component to make a direct fetch request to the API in useEffect - Provided solutions for potential issues with connecting to localhost from a mobile emulator

--- src/Redux/reduxHook.ts
+++ src/Redux/reduxHook.ts
@@ -1,6 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from '../Redux/store/store';
 
-// Usar en lugar de useDispatch y useSelector en los componentes
-export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
